777294d2dbe858deea145f5af09db0c8afadbb7a
Sean <seansherlock18@gmail.com>
2018-07-18 22:16:34 -0700

    reworked table

--- app.js
+++ app.js
@@ -35,7 +35,7 @@
 
 // var timeTotal = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm', 'Total'];
 
-
+1
 // var ulEl = document.createElement('ul');
 // for(var i = 0; i < timeTotal.length; i++) {
 //   var liEl = document.createElement('li');
