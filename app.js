3d5cd2fc70e321561198b55eebcc4fb58029027c
Sean <seansherlock18@gmail.com>
2018-07-20 08:44:34 -0700

    changed style slightly

--- app.js
+++ app.js
@@ -3,7 +3,6 @@
 
 
 
-=======
 // var timeTotal = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 // var allCookieShops = [];
 // function CookieGen(minCust, maxCust, averageCookie, total, location) {
